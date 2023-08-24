7784932949e5f65e799fed2e6709e91ab201ade0
Maxim Lesev <lesov0iblr@gmail.com>
2023-08-24 15:10:02 +0200

    created the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,15 @@
 import {combineReducers} from "redux";
+import {configureStore} from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
 
 })
+
+export const store = configureStore({
+    reducer: rootReducer
+})
+
+// This lets me look at the project's state
+// @ts-ignore
+window.store = store;
 
