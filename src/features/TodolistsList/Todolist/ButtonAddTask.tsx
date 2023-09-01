82117fa72672a24616226b33d1a5d788341f6df0
Maxim Lesev <lesov0iblr@gmail.com>
2023-09-01 09:37:55 +0200

    change button - add task

--- src/features/TodolistsList/Todolist/ButtonAddTask.tsx
+++ src/features/TodolistsList/Todolist/ButtonAddTask.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from "react";
 import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
+import {FormControlLabel, FormGroup} from "@mui/material";
 
 type ButtonAddTaskPropsType = {
     onClick: () => void
@@ -9,14 +10,23 @@ type ButtonAddTaskPropsType = {
 export const ButtonAddTask: FC<ButtonAddTaskPropsType> = (props) => {
     return (
         <>
-            <IconButton
-                color={"inherit"}
-                size={'small'}
-                aria-label={'Add task'}
-                onClick={props.onClick}
-                >
-                <AddIcon/>
-            </IconButton>
+            <FormGroup
+                sx={{ p: 2 }}
+            >
+                <FormControlLabel
+                    control={
+                        <IconButton
+                            color={"inherit"}
+                            size={'small'}
+                            aria-label={'Add task'}
+                            onClick={props.onClick}
+                        >
+                            <AddIcon/>
+                        </IconButton>
+                    }
+                    label={'Add task'}
+                />
+            </FormGroup>
         </>
     )
 }
