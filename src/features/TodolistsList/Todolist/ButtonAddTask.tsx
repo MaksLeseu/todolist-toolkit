724af1397f44dec993d4d14f3e77e79c85e0f70d
Maxim Lesev <lesov0iblr@gmail.com>
2023-09-01 13:58:31 +0200

    change styles

--- src/features/TodolistsList/Todolist/ButtonAddTask.tsx
+++ src/features/TodolistsList/Todolist/ButtonAddTask.tsx
@@ -2,6 +2,7 @@ import React, {FC} from "react";
 import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
 import {FormControlLabel, FormGroup} from "@mui/material";
+import s from './Task/Task.module.css'
 
 type ButtonAddTaskPropsType = {
     onClick: () => void
@@ -11,20 +12,22 @@ export const ButtonAddTask: FC<ButtonAddTaskPropsType> = (props) => {
     return (
         <>
             <FormGroup
-                sx={{ p: 2 }}
+                sx={{ paddingLeft: 2, paddingTop: 2, }}
             >
                 <FormControlLabel
                     control={
                         <IconButton
-                            color={"inherit"}
+                            color={"default"}
                             size={'small'}
                             aria-label={'Add task'}
                             onClick={props.onClick}
+                            disableRipple={true}
                         >
                             <AddIcon/>
                         </IconButton>
                     }
-                    label={'Add task'}
+                    label={'Add a task'}
+                    className={s.btnAddTask}
                 />
             </FormGroup>
         </>
