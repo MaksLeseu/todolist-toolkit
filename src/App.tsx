1c3fd11f947d8747e91a5c7f001af24c3037c4c1
Maxim Lesev <lesov0iblr@gmail.com>
2023-09-01 14:17:39 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
 import {TodolistsList} from "./features/TodolistsList/TodolistsList";
+import {
+    AppBar,
+    Container,
+    CssBaseline, FormControlLabel,
+    FormGroup,
+    Grid, IconButton,
+    Toolbar,
+    Typography,
+} from "@mui/material";
 
 export const App = () => {
   return (
     <div className="App">
       <div className="container">
+          <AppBar position={'static'} sx={{marginBottom: '40px'}}>
+              <Toolbar>
+                  <Typography
+                      variant={'h6'}
+                      component={'div'}
+                      sx={{flexGrow: 1}}
+                  >
+                      Todolist
+                  </Typography>
+              </Toolbar>
+          </AppBar>
         <TodolistsList />
       </div>
     </div>
