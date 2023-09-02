a4b789598037ff4c98e054cdd32e8aee64bbe55e
Maxim Lesev <lesov0iblr@gmail.com>
2023-09-02 21:49:14 +0200

    add collecting data from input fields

--- src/features/Auth/Login.tsx
+++ src/features/Auth/Login.tsx
@@ -1,12 +1,29 @@
 import {Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid} from "@mui/material";
 import TextField from "@mui/material/TextField";
+import {useFormik} from "formik";
+import {useAppDispatch} from "../../common/hooks/useAppDispatch";
 
 export const Login = () => {
+    const dispatch = useAppDispatch()
 
+    const formik = useFormik( {
+        validate: (values) => {
+            if (!values.email) return { email: '' }
+            if (!values.password) return { password: '' }
+        },
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values))
+        },
+    })
 
     return <Grid container justifyContent="center">
         <Grid item xs={4}>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <FormControl>
                     <FormLabel>
                         <p>
@@ -26,16 +43,19 @@ export const Login = () => {
                         <TextField
                             label="Email"
                             margin="normal"
+                            {...formik.getFieldProps("email")}
                         />
                         <TextField
                             type="password"
                             label="Password"
                             margin="normal"
+                            {...formik.getFieldProps("password")}
                         />
                         <FormControlLabel
                             label={'Remember me'}
                             control={<Checkbox
-
+                                {...formik.getFieldProps("rememberMe")}
+                                checked={formik.values.rememberMe}
                             />}
                         />
                         <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
