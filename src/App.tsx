3348cabc72fa42b21361172253d9a5e27260b69d
Maxim Lesev <lesov0iblr@gmail.com>
2023-09-13 08:06:52 +0200

    add component BaseMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,24 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import {TodolistsList} from "./features/TodolistsList/TodolistsList";
-import {
-    AppBar, Button, IconButton,
-    Toolbar,
-    Typography,
-} from "@mui/material";
-import {Menu} from "@mui/icons-material";
-import {useAppDispatch} from "./common/hooks/useAppDispatch";
-import {authThunk} from "./features/Auth/auth-slice";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Login} from "./features/Auth/Login";
-import {useSelector} from "react-redux";
-import {AppRootStateType} from "./app/store";
+import {BaseMenu} from "./app/BaseMenu";
+import {useAppDispatch} from "./common/hooks/useAppDispatch";
+import {authThunk} from "./features/Auth/auth-slice";
 
 export const App = () => {
-    const isLoggedIn = useSelector((state: AppRootStateType) => state.auth.isLoggedIn)
+
     const dispatch = useAppDispatch()
 
     useEffect(() => {
         dispatch(authThunk.authMe({}))
     }, [])
 
-    const handlerLogout = () => dispatch(authThunk.logout({}))
-
     return (
         <BrowserRouter>
             <div className="container">
-                <AppBar position={'static'} sx={{marginBottom: '40px'}}>
-                    <Toolbar>
-                        <IconButton
-                            size={'large'}
-                            edge={'start'}
-                            color={'inherit'}
-                            aria-label={'menu'}
-                            sx={{mr: 2}}
-                        >
-                            <Menu/>
-                        </IconButton>
-                        <Typography
-                            variant={'h6'}
-                            component={'div'}
-                            sx={{flexGrow: 1}}
-                        >
-                            Todolist
-                        </Typography>
-                        {
-                            isLoggedIn
-                                ?
-                                <Button
-                                color={'inherit'}
-                                onClick={handlerLogout}
-                            >
-                                Log out
-                            </Button>
-                                :
-                            null
-                        }
-                    </Toolbar>
-                </AppBar>
+                <BaseMenu />
                 <Routes>
                     <Route path={'/'} element={<TodolistsList/>}/>
                     <Route path={'/login'} element={<Login/>}/>
