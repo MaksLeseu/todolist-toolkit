85025cb0720764108ef809d22b86c1f5da18b4ac
Maxim Lesev <lesov0iblr@gmail.com>
2023-10-09 17:41:58 +0200

    change the header in api

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': '6c38e33f-71de-49e1-a68d-149628854242'
     }
 })
