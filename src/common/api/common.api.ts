1b9ecdce7eed78cfe25214ef979fbd0fa4d0d9e7
Maxim Lesev <lesov0iblr@gmail.com>
2023-10-10 07:36:23 +0200

    add my API-KEY in header

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '77e9ebd2-efbf-4b5a-82fe-4b2ad908357b'
+        'API-KEY': '6c38e33f-71de-49e1-a68d-149628854242'
     }
 })
